536739624f179103a514af7bf6e78a6922ae2f0b
Tanaka <pyontana@gmail.com>
2019-06-09 06:35:37 +0000

    エラーでexitするようにする

--- thom_con_idx.rb
+++ thom_con_idx.rb
@@ -3,15 +3,16 @@ suM, cnT = gets.chomp.split(" ").map(&:to_i)
 
 if (sum < cnt || suM < cnT)
     puts "分子よりも大きい分母は入力できません"
+    exit
 end
-
-a = (sum * cnT).to_f
-b = (suM * cnt).to_f
 
 if (sum >= suM ||  cnt >= cnT)
     puts "入力した数値に誤りがあります"
     exit
 end
+
+a = (sum * cnT).to_f
+b = (suM * cnt).to_f
 
 ans = 0
 ans = (a / b).to_f
