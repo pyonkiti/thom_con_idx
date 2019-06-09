94fe1c6b7c80ab47348df0704ddc91afbc89e17d
Tanaka <pyontana@gmail.com>
2019-06-09 06:27:36 +0000

    初回登録

---
+++ thom_con_idx.rb
@@ -0,0 +1,18 @@
+sum, cnt = gets.chomp.split(" ").map(&:to_i)
+suM, cnT = gets.chomp.split(" ").map(&:to_i)
+
+if (sum < cnt || suM < cnT)
+    puts "分子よりも大きい分母は入力できません"
+end
+
+a = (sum * cnT).to_f
+b = (suM * cnt).to_f
+
+if (sum >= suM ||  cnt >= cnT)
+    puts "入力した数値に誤りがあります"
+    exit
+end
+
+ans = 0
+ans = (a / b).to_f
+puts ans
